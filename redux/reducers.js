9cd2e97fb70de8609d3b4c8a605e851d33dd69fd
Wen Yuan <wenyuanchin229@gmail.com>
2022-02-07 11:51:27 +0800

    integrated redux-saga, extracted images

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,45 +1,55 @@
-import { GET_MOVIES, ADD_FAVORITE_ITEM, REMOVE_FAVORITE_ITEM, LOAD_IMG, ADD_LOVE, UNLOVE } from "./actions";
+import {
+  ADD_FAVORITE_ITEM,
+  REMOVE_FAVORITE_ITEM,
+  LOAD_IMG,
+  ADD_LOVE,
+  UNLOVE,
+  GET_MOVIES_SUCCESS,
+} from './actions';
 const initialState = {
-    movies: [],
-    favorites: [],
-    pictures: []
+  movies: [],
+  favorites: [],
+  pictures: [],
 };
 function moviesReducer(state = initialState, action) {
-    switch (action.type) {
-        case GET_MOVIES:
-            return { ...state, movies: action.payload };
-        case ADD_FAVORITE_ITEM:
-            return { ...state, favorites: [...state.favorites, action.payload] };
-        case REMOVE_FAVORITE_ITEM:
-            return {
-                ...state,
-                favorites: state.favorites.filter(
-                    movie => movie.id !== action.payload.id,
-                ),
-            };
-        case LOAD_IMG:
-            return {
-                ...state,
-                pictures: action.payload
-            };
-        case ADD_LOVE:
-            return {
-                ...state,
-                pictures: state.pictures.map(
-                    (content, i) => i === action.payload.id ? { ...content, love: action.payload.num, pressed: true }
-                        : content
-                )
-            };
-        case UNLOVE:
-            return {
-                ...state,
-                pictures: state.pictures.map(
-                    (content, i) => i === action.payload.id ? { ...content, love: action.payload.num, pressed: false }
-                        : content
-                )
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case GET_MOVIES_SUCCESS:
+      console.log('reducer hit with payload: ', action.payload);
+      return {...state, movies: action.payload};
+    case ADD_FAVORITE_ITEM:
+      return {...state, favorites: [...state.favorites, action.payload]};
+    case REMOVE_FAVORITE_ITEM:
+      return {
+        ...state,
+        favorites: state.favorites.filter(
+          movie => movie.id !== action.payload.id,
+        ),
+      };
+    case LOAD_IMG:
+      return {
+        ...state,
+        pictures: action.payload,
+      };
+    case ADD_LOVE:
+      return {
+        ...state,
+        pictures: state.pictures.map((content, i) =>
+          i === action.payload.id
+            ? {...content, love: action.payload.num, pressed: true}
+            : content,
+        ),
+      };
+    case UNLOVE:
+      return {
+        ...state,
+        pictures: state.pictures.map((content, i) =>
+          i === action.payload.id
+            ? {...content, love: action.payload.num, pressed: false}
+            : content,
+        ),
+      };
+    default:
+      return state;
+  }
 }
 export default moviesReducer;
--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,14 @@
+/* eslint-disable prettier/prettier */
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import moviesReducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 const rootReducer = combineReducers({
   moviesReducer,
 });
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
